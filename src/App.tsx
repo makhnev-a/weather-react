673f429e282579f3c55424bbec8681405074d5b8
makhnev-a <ray.makhnev@gmail.com>
2020-08-17 17:32:56 +0300

    add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
+const api = {
+    key: '330216f9e3042b8a57a7865c3de67865',
+    base: 'api.openweathermap.org/data/2.5/'
+};
+
 function App() {
-  return (
-    <div className="app">
-      app
-    </div>
-  );
+    const dateBuilder = (d: any) => {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`;
+    };
+
+    return (
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className={'search-bar'}
+                        placeholder={'Search...'}
+                    />
+                </div>
+                <div className="location-box">
+                    <div className="location">New York City, US</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="weather-box">
+                    <div className="temp">15°c</div>
+                    <div className="weather">Sunny</div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
